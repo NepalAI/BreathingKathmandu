94a7aad453d267291e8a17f88998577ab3923529
Akash Adhikari <akash_532998@yahoo.com>
2017-08-16 06:08:05 +0545

    playing with gui

--- SkyScraper.py
+++ SkyScraper.py
@@ -1,6 +1,10 @@
 import sys
+from PyQt5.QtCore import QCoreApplication
+# from PyQt5.QtGui import *
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QAction
+from PyQt5.uic.properties import QtGui
+
 
 class window(QMainWindow):
 
@@ -9,8 +13,42 @@ class window(QMainWindow):
         self.setGeometry(50, 50, 500, 300)
         self.setWindowTitle('SkyScraper')
         # self.setWindowIcon(QIcon('pic.png'))
+
+        extractAction = QAction('&Exit', self)
+        extractAction.setShortcut('Ctrl+Q')
+        extractAction.setStatusTip('leave the app')
+        extractAction.triggered.connect(self.close_application)
+
+        self.statusBar()
+
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu('&File')
+        fileMenu.addAction(extractAction)
+
+        extractAction = QAction(QIcon('pic.png'), 'flee the scene', self)
+        extractAction.triggered.connect(self.close_application)
+
+        self.toolBar = self.addToolBar('Extraction')
+        self.toolBar.addAction(extractAction)
+
+        self.home()
+
+    def home(self):
+        btn = QPushButton('quit', self)
+        btn.clicked.connect(self.close_application)
+        btn.resize(btn.sizeHint())
+        btn.move(0, 100)
+
         self.show()
 
-app = QApplication(sys.argv)
-Gui = window()
-sys.exit(app.exec_())
+    def close_application(self):
+        print('whooo so custom')
+        sys.exit()
+
+if __name__ == "__main__":
+    def run():
+        app = QApplication(sys.argv)
+        Gui = window()
+        sys.exit(app.exec_())
+
+run()
