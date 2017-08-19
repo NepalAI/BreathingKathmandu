de58a304dbdc6f57dba28271d44916d909d9e1d7
Akash Adhikari <akash_532998@yahoo.com>
2017-08-19 21:49:16 +0545

    tkinter oop

--- SkyScraper.py
+++ SkyScraper.py
@@ -1,10 +1,39 @@
-from tkinter import *
+import tkinter as tk
 
-class Window(Frame):
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.master = master
+LARGE_FONT = ("Verdana", 12)
 
-root = Tk()
-app = Window(root)
-root.mainloop()
+# we're inheriting everything from the tk.Tk class
+class SeaofBTCapp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand=True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        frame = StartPage(container, self)
+
+        self.frames[StartPage] = frame
+
+        frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="SkyScraper", font=LARGE_FONT)
+        label.pack(pady=100, padx=100)
+
+
+app = SeaofBTCapp()
+app.mainloop()
