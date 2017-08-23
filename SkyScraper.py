83c06167d210cade4851105ce2ff9b5359c89a0f
Akash Adhikari <akash_532998@yahoo.com>
2017-08-23 21:38:04 +0545

    some more documentation -- exam time

--- SkyScraper.py
+++ SkyScraper.py
@@ -3,47 +3,64 @@ import tkinter as tk
 LARGE_FONT = ("Verdana", 12)
 
 # Define SeaofBTCapp class
+
 # We're inheriting everything from the tk.Tk class.
 
 class SeaofBTCapp(tk.Tk):
 
     # Init is short of initialize.
+
     # Whatever you put in here is going to always run whenever the class is called upon.
+
     # The other methods will only run when you specifically call upon them to run.
 
     def __init__(self, *args, **kwargs):
 
         # https://stackoverflow.com/questions/36901/what-does-double-star-and-star-do-for-parameters
+
         # https://stackoverflow.com/questions/287085/what-do-args-and-kwargs-mean#287101
+
         tk.Tk.__init__(self, *args, **kwargs)
+
+        # We've defined this container, which will be filled with a bunch of frames to be accessed later on.
 
         container = tk.Frame(self)
 
         container.pack(side="top", fill="both", expand=True)
 
         container.grid_rowconfigure(0, weight=1)
+
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
 
         for F in (StartPage, PageOne, PageTwo):
+
             frame = F(container, self)
 
             self.frames[F] = frame
 
+            # "nsew" corresponds to directions (north, south, east, west).
+            
             frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(StartPage)
 
     def show_frame(self, cont):
+
         frame = self.frames[cont]
+
         frame.tkraise()
 
 
 class StartPage(tk.Frame):
+
     def __init__(self, parent, controller):
+
         tk.Frame.__init__(self, parent)
+
         label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+
         label.pack(pady=10, padx=10)
 
         button = tk.Button(self, text="Visit Page 1",
@@ -56,9 +73,13 @@ class StartPage(tk.Frame):
 
 
 class PageOne(tk.Frame):
+
     def __init__(self, parent, controller):
+
         tk.Frame.__init__(self, parent)
+
         label = tk.Label(self, text="Page One!!!", font=LARGE_FONT)
+
         label.pack(pady=10, padx=10)
 
         button1 = tk.Button(self, text="Back to Home",
@@ -71,9 +92,13 @@ class PageOne(tk.Frame):
 
 
 class PageTwo(tk.Frame):
+
     def __init__(self, parent, controller):
+
         tk.Frame.__init__(self, parent)
+
         label = tk.Label(self, text="Page Two!!!", font=LARGE_FONT)
+
         label.pack(pady=10, padx=10)
 
         button1 = tk.Button(self, text="Back to Home",
@@ -86,4 +111,5 @@ class PageTwo(tk.Frame):
 
 
 app = SeaofBTCapp()
+
 app.mainloop()
