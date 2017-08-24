7697be4f609c41b299c6b20ffd097fde95304918
Akash Adhikari <akash_532998@yahoo.com>
2017-08-24 19:29:43 +0545

    Adding plots with Matplotlib

--- SkyScraper.py
+++ SkyScraper.py
@@ -1,4 +1,9 @@
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
 import tkinter as tk
+from tkinter import ttk
 
 LARGE_FONT = ("Verdana", 12)
 
@@ -6,9 +11,7 @@ LARGE_FONT = ("Verdana", 12)
 
 # We're inheriting everything from the tk.Tk class.
 
-class SeaofBTCapp(tk.Tk):
-
-    # Init is short of initialize.
+class BreathingKathmandu(tk.Tk):
 
     # Whatever you put in here is going to always run whenever the class is called upon.
 
@@ -22,6 +25,8 @@ class SeaofBTCapp(tk.Tk):
 
         tk.Tk.__init__(self, *args, **kwargs)
 
+        tk.Tk.wm_title(self, "Breathing Kathmandu")
+
         # We've defined this container, which will be filled with a bunch of frames to be accessed later on.
 
         container = tk.Frame(self)
@@ -34,14 +39,14 @@ class SeaofBTCapp(tk.Tk):
 
         self.frames = {}
 
-        for F in (StartPage, PageOne, PageTwo):
+        for F in (StartPage, PageOne, PageTwo, PageThree):
 
             frame = F(container, self)
 
             self.frames[F] = frame
 
             # "nsew" corresponds to directions (north, south, east, west).
-            
+
             frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(StartPage)
@@ -59,17 +64,21 @@ class StartPage(tk.Frame):
 
         tk.Frame.__init__(self, parent)
 
-        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label = ttk.Label(self, text="Start Page", font=LARGE_FONT)
 
         label.pack(pady=10, padx=10)
 
-        button = tk.Button(self, text="Visit Page 1",
+        button = ttk.Button(self, text="Visit Page 1",
                            command=lambda: controller.show_frame(PageOne))
         button.pack()
 
-        button2 = tk.Button(self, text="Visit Page 2",
+        button2 = ttk.Button(self, text="Visit Page 2",
                             command=lambda: controller.show_frame(PageTwo))
         button2.pack()
+
+        button3 = ttk.Button(self, text="Pollution",
+                             command=lambda: controller.show_frame(PageThree))
+        button3.pack()
 
 
 class PageOne(tk.Frame):
@@ -78,15 +87,15 @@ class PageOne(tk.Frame):
 
         tk.Frame.__init__(self, parent)
 
-        label = tk.Label(self, text="Page One!!!", font=LARGE_FONT)
+        label = ttk.Label(self, text="Page One!!!", font=LARGE_FONT)
 
         label.pack(pady=10, padx=10)
 
-        button1 = tk.Button(self, text="Back to Home",
+        button1 = ttk.Button(self, text="Back to Home",
                             command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
-        button2 = tk.Button(self, text="Page Two",
+        button2 = ttk.Button(self, text="Page Two",
                             command=lambda: controller.show_frame(PageTwo))
         button2.pack()
 
@@ -97,19 +106,41 @@ class PageTwo(tk.Frame):
 
         tk.Frame.__init__(self, parent)
 
-        label = tk.Label(self, text="Page Two!!!", font=LARGE_FONT)
+        label = ttk.Label(self, text="Page Two!!!", font=LARGE_FONT)
 
         label.pack(pady=10, padx=10)
 
-        button1 = tk.Button(self, text="Back to Home",
+        button1 = ttk.Button(self, text="Back to Home",
                             command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
-        button2 = tk.Button(self, text="Page One",
+        button2 = ttk.Button(self, text="Page One",
                             command=lambda: controller.show_frame(PageOne))
         button2.pack()
 
 
-app = SeaofBTCapp()
+class PageThree(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Pollution Chart", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = ttk.Button(self, text="Back to Home",
+                             command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        f = Figure(figsize=(5, 5), dpi=100)
+        a = f.add_subplot(111)
+        a.plot([1, 2, 3, 4, 5, 6, 7, 8], [5, 6, 1, 3, 8, 9, 3, 5])
+
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+        toolbar = NavigationToolbar2TkAgg(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+app = BreathingKathmandu()
 
 app.mainloop()
