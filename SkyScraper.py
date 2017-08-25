287136c6791516ac18b5b48a766ecbd09150d89d
Akash Adhikari <akash_532998@yahoo.com>
2017-08-25 21:22:29 +0545

    Realtime updating graph

--- SkyScraper.py
+++ SkyScraper.py
@@ -1,13 +1,34 @@
 import matplotlib
-matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
+import matplotlib.animation as animation
+from matplotlib import style
+style.use('ggplot')
 import tkinter as tk
 from tkinter import ttk
+matplotlib.use("TkAgg")
+
 
 LARGE_FONT = ("Verdana", 12)
 
-# Define SeaofBTCapp class
+f = Figure(figsize=(5,5), dpi=100)
+a = f.add_subplot(111)
+
+def animate(i):
+    pullData = open('sample.txt','r').read()
+    dataList = pullData.split('\n')
+    xList = []
+    yList = []
+    for eachLine in dataList:
+        if len(eachLine) > 1:
+            x, y = eachLine.split(',')
+            xList.append(int(x))
+            yList.append(int(y))
+
+    a.clear()
+    a.plot(xList, yList)
+
+# Define BreathingKathmandu class
 
 # We're inheriting everything from the tk.Tk class.
 
@@ -24,29 +45,22 @@ class BreathingKathmandu(tk.Tk):
         # https://stackoverflow.com/questions/287085/what-do-args-and-kwargs-mean#287101
 
         tk.Tk.__init__(self, *args, **kwargs)
-
         tk.Tk.wm_title(self, "Breathing Kathmandu")
 
         # We've defined this container, which will be filled with a bunch of frames to be accessed later on.
 
         container = tk.Frame(self)
-
         container.pack(side="top", fill="both", expand=True)
-
         container.grid_rowconfigure(0, weight=1)
-
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
 
         for F in (StartPage, PageOne, PageTwo, PageThree):
-
             frame = F(container, self)
-
             self.frames[F] = frame
 
             # "nsew" corresponds to directions (north, south, east, west).
-
             frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(StartPage)
@@ -54,7 +68,6 @@ class BreathingKathmandu(tk.Tk):
     def show_frame(self, cont):
 
         frame = self.frames[cont]
-
         frame.tkraise()
 
 
@@ -64,8 +77,7 @@ class StartPage(tk.Frame):
 
         tk.Frame.__init__(self, parent)
 
-        label = ttk.Label(self, text="Start Page", font=LARGE_FONT)
-
+        label = ttk.Label(self, text="Breathing Kathmandu", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
         button = ttk.Button(self, text="Visit Page 1",
@@ -88,7 +100,6 @@ class PageOne(tk.Frame):
         tk.Frame.__init__(self, parent)
 
         label = ttk.Label(self, text="Page One!!!", font=LARGE_FONT)
-
         label.pack(pady=10, padx=10)
 
         button1 = ttk.Button(self, text="Back to Home",
@@ -107,7 +118,6 @@ class PageTwo(tk.Frame):
         tk.Frame.__init__(self, parent)
 
         label = ttk.Label(self, text="Page Two!!!", font=LARGE_FONT)
-
         label.pack(pady=10, padx=10)
 
         button1 = ttk.Button(self, text="Back to Home",
@@ -120,8 +130,11 @@ class PageTwo(tk.Frame):
 
 
 class PageThree(tk.Frame):
+
     def __init__(self, parent, controller):
+
         tk.Frame.__init__(self, parent)
+
         label = tk.Label(self, text="Pollution Chart", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
@@ -129,18 +142,19 @@ class PageThree(tk.Frame):
                              command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
-        f = Figure(figsize=(5, 5), dpi=100)
-        a = f.add_subplot(111)
-        a.plot([1, 2, 3, 4, 5, 6, 7, 8], [5, 6, 1, 3, 8, 9, 3, 5])
+        # f = Figure(figsize=(5, 5), dpi=100)
+        #
+        # a = f.add_subplot(111)
+        # a.plot([1, 2, 3, 4, 5, 6, 7, 8], [5, 6, 1, 3, 8, 9, 3, 5])
 
         canvas = FigureCanvasTkAgg(f, self)
         canvas.show()
-        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
         toolbar = NavigationToolbar2TkAgg(canvas, self)
         toolbar.update()
-        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
 app = BreathingKathmandu()
-
+ani = animation.FuncAnimation(f,animate, interval=1000)
 app.mainloop()
