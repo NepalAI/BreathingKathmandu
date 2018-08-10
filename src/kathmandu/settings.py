b94c693fb8d0b5640fd3a2791a6bcc80efcc13ba
Akash Adhikari <akashsky1313@gmail.com>
2018-08-10 07:52:47 +0545

    Modify directory

--- src/kathmandu/settings.py
+++ src/kathmandu/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.interface'
+    'apps.interface',
+    'apps.deeplearing',
 ]
 
 MIDDLEWARE = [
